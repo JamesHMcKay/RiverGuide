5943d98102bc5fa21534da704b1fd4a1eab3169c
James McKay <jhmckay93@gmail.com>
2019-05-26 14:00:20 +1200

    added missing file

---
+++ src/actions/getGauges.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+import {
+    GET_GAUGES
+} from "./types";
+
+const serverLocation = process.env.REACT_APP_RIVER_SERVICE_URL;
+
+export const makeGaugeRequest = () => dispatch => {
+
+    const request = {
+        action: "get_features",
+        filters: ["flow"],
+        crossDomain: true,
+    }
+    axios
+        .post(serverLocation, request)
+        .then(res => {
+            let data = res.data.features;
+            let result = data.map(item => (
+                {
+                    id: item.id,
+                    gauge_id: item.id,
+                    display_name: item.name,
+                    position: {lat: item.location.lat, lon: item.location.lon },
+                    observables: item.observables,
+                    region: item.data_source,
+                    type: "gauge",
+                }));
+            dispatch({
+                type: GET_GAUGES,
+                payload: result,
+            });
+        });
+};
