0fb29ee2df7c74e46fe1a181079d1ab6736c0780
Fergus Farrell <fergusf@orionhealth.com>
2020-01-26 09:38:30 +1300

    removed unused bootstrap library

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 // import * as serviceWorker from "./serviceWorker";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/font-awesome/css/font-awesome.min.css";
 import { unregister } from './serviceWorker';
 
