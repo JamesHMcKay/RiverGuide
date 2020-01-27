c0b48c1adcef215a774e24695534d0cc67145c73
James McKay <jhmckay93@gmail.com>
2020-01-27 20:24:24 +1300

    improve clustering UI

--- src/components/map/MapCluster.tsx
+++ src/components/map/MapCluster.tsx
@@ -11,13 +11,7 @@ export interface IMapClusterProps {
 
 export default class MapCluster extends PureComponent<IMapClusterProps> {
     public render(): JSX.Element {
-        let size: number = this.props.size > 10 ? this.props.size : 10;
-        if (size > 20) {
-            size = 20;
-        }
-        const padding: string = size > 10 ? "20%" : "none";
-        const width: number = size * 3;
-        const borderRadius: number = size * 3 / 2;
+        const width: number = this.props.size * 4;
         return (
             <div
                 className="map-cluster"
@@ -27,13 +21,8 @@ export default class MapCluster extends PureComponent<IMapClusterProps> {
                     this.props.longitude,
                 )}
                 style={{
-                    transform: `translate(${-size / 2}px,${-size}px)`,
-                    color: "white",
                     width: `${width}px`,
-                    height: `${width}px`,
-                    borderRadius: `${borderRadius}px`,
-                    paddingTop: padding,
-                    cursor: "pointer",
+                    height: `${width}px`
                 }}
             >
                 {this.props.count}
